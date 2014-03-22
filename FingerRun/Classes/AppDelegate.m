8d81f7d9a9907538a1200fa54b5ba21e2ab86103
Samuel Ward <samuel.ward@me.com>
2014-03-22 11:50:08 +0000

    Initial commit

---
+++ FingerRun/Classes/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  FingerRun
+//
+//  Created by Samuel Ward on 22/03/2014.
+//  Copyright (c) 2014 Samuel Ward. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    return YES;
+}
+
+@end
