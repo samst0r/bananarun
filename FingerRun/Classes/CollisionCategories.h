211fc994c36c21154efe6cff2e3120e529d212b4
Samuel Ward <samuel.ward@me.com>
2014-03-22 23:35:56 +0000

    Added bananas

---
+++ FingerRun/Classes/CollisionCategories.h
@@ -0,0 +1,13 @@
+//
+//  CollisionCategories.h
+//  FingerRun
+//
+//  Created by Samuel Ward on 22/03/2014.
+//  Copyright (c) 2014 Samuel Ward. All rights reserved.
+//
+
+typedef NS_ENUM(uint8_t, Collider) {
+    
+    ColliderTypeBanana    = 1,
+    ColliderTypeFootprint = 2,
+};
--- FingerRun/Classes/Scenes/Scene.m
+++ FingerRun/Classes/Scenes/Scene.m
@@ -11,16 +11,23 @@
 #import "BackgroundSpriteNode.h"
 #import "FootPrintSpriteNode.h"
 #import "RoadMarkerSpriteNode.h"
+#import "BananaObsticleSpriteNode.h"
 
-@interface Scene ()
+@interface Scene () <SKPhysicsContactDelegate>
 
 @property (nonatomic) BackgroundSpriteNode *background;
 @property (nonatomic) RoadMarkerSpriteNode *roadMarkerNode;
 
 @property (nonatomic) SKLabelNode *speedLabel;
 @property (nonatomic) SKLabelNode *topSpeedLabel;
+@property (nonatomic) SKLabelNode *highestAllTimeSpeedLabel;
 
 @property (nonatomic) NSInteger topSpeed;
+@property (nonatomic) NSInteger highestAllTimeSpeed;
+@property (nonatomic) NSInteger movementSpeed;
+
+@property NSTimeInterval timeOfLastMove;
+@property NSTimeInterval timePerMove;
 
 @end
 
@@ -28,10 +35,23 @@
 
 #pragma mark - Initialisation
 
+- (void)setupAndAddHighestSpeedLabel {
+    
+    _highestAllTimeSpeedLabel = [[SKLabelNode alloc] initWithFontNamed:@"Courier"];
+    _highestAllTimeSpeedLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetHeight(self.frame) - 40.0f);
+    _highestAllTimeSpeedLabel.fontSize = 14.0f;
+    _highestAllTimeSpeedLabel.text = @"";
+    _highestAllTimeSpeedLabel.fontColor = [UIColor redColor];
+    _highestAllTimeSpeedLabel.alpha = 0.85f;
+    
+    [self addChild:_highestAllTimeSpeedLabel];
+}
+
 - (void)setupAndAddSpeedLabel {
     
-    _speedLabel = [[SKLabelNode alloc] initWithFontNamed:@"Helvetica"];
-    _speedLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetHeight(self.frame) - 80);
+    _speedLabel = [[SKLabelNode alloc] initWithFontNamed:@"Courier"];
+    _speedLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetHeight(self.frame) - 80.0f);
+    _speedLabel.fontSize = 18.0f;
     _speedLabel.text = @"";
     _speedLabel.fontColor = [UIColor yellowColor];
     _speedLabel.alpha = 0.85f;
@@ -41,27 +61,46 @@
 
 - (void)setupAndAddTopSpeedLabel {
     
-    _topSpeedLabel = [[SKLabelNode alloc] initWithFontNamed:@"Helvetica"];
-    _topSpeedLabel.fontSize = 8.0f;
-    _topSpeedLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetHeight(self.frame) - 96);
+    _topSpeedLabel = [[SKLabelNode alloc] initWithFontNamed:@"Courier"];
+    _topSpeedLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetHeight(self.frame) - 100.0f);
+    _topSpeedLabel.fontSize = 14.0f;
     _topSpeedLabel.text = @"";
-    _topSpeedLabel.fontColor = [UIColor yellowColor];
+    _topSpeedLabel.fontColor = [UIColor greenColor];
     _topSpeedLabel.alpha = 0.85f;
     
     [self addChild:_topSpeedLabel];
 }
 
--(id)initWithSize:(CGSize)size {
+- (id)initWithSize:(CGSize)size {
     
     if (self = [super initWithSize:size]) {
     
-        [self setupAndAddBackground];
-        [self setupAndAddRoadMarker];
-        [self setupAndAddSpeedLabel];
-        [self setupAndAddTopSpeedLabel];
+        self.timeOfLastMove = 0.0;
+        self.movementSpeed = 0.0;
     }
     
     return self;
+}
+
+- (void)addBanana {
+    BananaObsticleSpriteNode *banana = [[BananaObsticleSpriteNode alloc] init];
+    banana.position = CGPointMake(arc4random_uniform(CGRectGetWidth(self.frame)), CGRectGetHeight(self.frame) + 50.0f);
+    [self addChild:banana];
+}
+
+- (void) didMoveToView:(SKView *)view {
+ 
+    [self setupAndAddBackground];
+    [self setupAndAddRoadMarker];
+    [self setupAndAddSpeedLabel];
+    [self setupAndAddTopSpeedLabel];
+    [self setupAndAddHighestSpeedLabel];
+    
+    self.physicsWorld.contactDelegate = self;
+
+    [self addBanana];
+    
+    self.timePerMove = 1.0;
 }
 
 - (void)setupAndAddBackground {
@@ -82,61 +121,59 @@
     _roadMarkerNode = [[RoadMarkerSpriteNode alloc] init];
     _roadMarkerNode.position = CGPointMake(CGRectGetMidX(self.frame), 0);
     _roadMarkerNode.size = CGSizeMake(20, 80);
-    _roadMarkerNode.speed = 0.0f;
     
     [self addChild:_roadMarkerNode];
 }
-
-//- (void)setupRoadMarker {
-//    
-//    //1
-//    NSArray *parallaxBackgroundNames = @[@"bg_galaxy.png", @"bg_planetsunrise.png",
-//                                         @"bg_spacialanomaly.png", @"bg_spacialanomaly2.png"];
-//    CGSize planetSizes = CGSizeMake(200.0, 200.0);
-//    //2
-//    _parallaxNodeBackgrounds = [[ alloc] initWithBackgrounds:parallaxBackgroundNames
-//                                                                       size:planetSizes
-//                                                       pointsPerSecondSpeed:10.0];
-//    //3
-//    _parallaxNodeBackgrounds.position = CGPointMake(size.width/2.0, size.height/2.0);
-//    //4
-//    [_parallaxNodeBackgrounds randomizeNodesPositions];
-//    
-//    //5
-//    [self addChild:_parallaxNodeBackgrounds];
-//    
-//    //6
-//    NSArray *parallaxBackground2Names = @[@"bg_front_spacedust.png",@"bg_front_spacedust.png"];
-//    _parallaxSpaceDust = [[FMMParallaxNode alloc] initWithBackgrounds:parallaxBackground2Names
-//                                                                 size:size
-//                                                 pointsPerSecondSpeed:25.0];
-//    _parallaxSpaceDust.position = CGPointMake(0, 0);
-//    [self addChild:_parallaxSpaceDust];
-//    
-//}
 
 #pragma mark - Scene Update
 - (void)update:(NSTimeInterval)currentTime {
     
     [self moveRoadMarker];
+    [self moveBananaObsticles:currentTime];
     
     [self recordSpeed];
-    [self recordTopSpeed];
+    [self recordCurrentTopSpeed];
+    [self recordHighestAllTimeSpeed];
+    
+    if (self.children.count < 13) {
+        
+        [self addBanana];
+    }
+    
+    if ((currentTime - self.timeOfLastMove > 1.0) && self.movementSpeed > 0) {
+        
+        self.movementSpeed -= 1;
+        self.timeOfLastMove = currentTime;
+    }
+    
 }
 
 #pragma mark - Scene Update Helpers
 - (void)moveRoadMarker {
-    self.roadMarkerNode.position = CGPointMake(self.roadMarkerNode.position.x,
-                                               self.roadMarkerNode.position.y + self.roadMarkerNode.speed);
     
-    if (self.roadMarkerNode.position.y >= self.background.size.height) {
+    self.roadMarkerNode.position = CGPointMake(self.roadMarkerNode.position.x,
+                                               self.roadMarkerNode.position.y - self.movementSpeed);
+    
+    if (self.roadMarkerNode.position.y <= 0) {
         
-        self.roadMarkerNode.position = CGPointMake(self.roadMarkerNode.position.x,
-                                                   0);
+        self.roadMarkerNode.position = CGPointMake(self.roadMarkerNode.position.x, CGRectGetHeight(self.frame) + self.roadMarkerNode.position.y);
     }
 }
 
 #pragma mark - Obsticle Movement Helpers
+
+- (void)moveBananaObsticles:(NSTimeInterval)currentTime {
+    
+    [self enumerateChildNodesWithName:@"Banana" usingBlock:^(SKNode *node, BOOL *stop) {
+        
+        if (node.position.y < 0) {
+            
+            [node removeFromParent];
+        }
+        
+        node.position = CGPointMake(node.position.x, node.position.y - self.movementSpeed);
+    }];
+}
 
 #pragma mark - User Tap Helpers
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -147,10 +184,6 @@
     FootPrintSpriteNode *footprint = [[FootPrintSpriteNode alloc] init];
     footprint.position = positionInScene;
     
-    footprint.size = CGSizeMake(50, 90);
-    footprint.xScale = 0.5;
-    footprint.yScale = 0.5;
-    
     [footprint hideAfterOneSecondsWithCompletion:^{
         
         [self removeChildrenInArray:@[footprint]];
@@ -159,34 +192,57 @@
     
     [self addChild:footprint];
     
-    self.roadMarkerNode.speed++;
+    self.movementSpeed++;
     
-    double delayInSeconds = 1.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        
-        if (self.roadMarkerNode.speed > 0) {
-            
-            self.roadMarkerNode.speed -= 1.0;
-        }
-    });
+    self.timeOfLastMove = event.timestamp;
 }
 
 #pragma mark - HUD Helpers
-- (void)recordTopSpeed {
-    if (self.roadMarkerNode.speed > self.topSpeed) {
+- (void)recordCurrentTopSpeed {
+    
+    if (self.movementSpeed > self.topSpeed) {
         
-        self.topSpeed = self.roadMarkerNode.speed;
-        self.topSpeedLabel.text = [NSString stringWithFormat:@"Top Speed %.02f MPH", self.roadMarkerNode.speed];
+        self.topSpeed = self.movementSpeed;
+        self.topSpeedLabel.text = [NSString stringWithFormat:@"Top Speed: %li MPH", self.movementSpeed];
+    }
+}
+
+- (void)recordHighestAllTimeSpeed {
+    
+    if (self.topSpeed > self.highestAllTimeSpeed) {
+        
+        self.highestAllTimeSpeed = self.topSpeed;
+        self.highestAllTimeSpeedLabel.text = [NSString stringWithFormat:@"BEST SPEED %li MPH", self.highestAllTimeSpeed];
     }
 }
 
 - (void)recordSpeed {
-    self.speedLabel.text = [NSString stringWithFormat:@"%.02f MPH", self.roadMarkerNode.speed];
+    
+    self.speedLabel.text = [NSString stringWithFormat:@"%li MPH", self.movementSpeed];
 }
 
 #pragma mark - Physics Contact Helpers
+- (void)didBeginContact:(SKPhysicsContact *)contact {
+    
+    BananaObsticleSpriteNode *node = (BananaObsticleSpriteNode *)contact.bodyA.node;
+    FootPrintSpriteNode *footPrint = (FootPrintSpriteNode *)contact.bodyA.node;
+
+    if (footPrint.alpha == 1.0f) {
+        
+        node.size = CGSizeMake(100.0f, 100.0f);
+        self.topSpeed = 0;
+        self.movementSpeed = 0;
+        
+        [node hideAfterOneSecondsWithCompletion:^{
+            
+            [node removeFromParent];
+        }];
+    }
+}
 
 #pragma mark - Game End Helpers
+- (void)gameOver {
+    
+}
 
 @end
